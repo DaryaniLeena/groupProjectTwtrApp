d771ad3056d04f6115c4ac12909a737883ea44bd
DaryaniLeena <daryani.l@husky.neu.edu>
2021-04-01 09:57:07 -0700

    added build folder

---
+++ twtr-jwt-master/fe/build/precache-manifest.5c2b3b20b3caf1c3d9d1c1d5468cb96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "729e9da10b92f7eb894140b022641ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a8f67641c74298ff0c1",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "70b80edd643775e66359",
+    "url": "/static/js/2.4eb74f49.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.4eb74f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8f67641c74298ff0c1",
+    "url": "/static/js/main.7202d44d.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
